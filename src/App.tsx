e65eb7aabc90c7d566bd06b00dbd1a4d03d533a5
jun <jun@Ubt2200.aaaaa>
2023-12-16 18:27:39 +0900

    canvas生成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import * as THREE from 'three';
 
-function App() {
+// サイズを指定
+const DISPLAY_WIDTH  = 960;
+const DISPLAY_HEIGHT = 540;
+
+const App = () => {
+  const canvasRef   = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+        //レンダラーを作成
+        const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+          canvas: canvasRef.current??undefined,
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+    }, [])
+
   return (
     <div className="App">
+        <canvas width={DISPLAY_WIDTH} height={DISPLAY_HEIGHT} ref={canvasRef} />
       hello world!!
     </div>
   );
